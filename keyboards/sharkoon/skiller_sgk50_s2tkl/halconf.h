4e08d01aebeef14793581738462450634ce3db4a
ReeViiS69 <wassermann.fabi@gmail.com>
2026-08-23 23:49:41 +0200

    add my keyboard, sharkoon tweeks, gpio dma rgb
    full credit for driver: https://github.com/emolitor/WestberryTech-WB32/blob/main/examples/ws2812-gpio-dma/ws2812_custom.c
    
    chatgpt chat used with manual adjustments for sharkoons macrofeature and custom rgb effect that i always used on my previous keyboards.
    its a shame sharkoon didnt include the macrofeature for their opensource release, only in their provided .hex firmware. i couldnt get qmks dynamic macro to work. so i utilise m0 and m1 VIA macroslots and custom(64-66) keys for recM0, recM1, stoprec. sharkoons rgb logic for recording macros included, also for capslock white while active.

---
+++ keyboards/sharkoon/skiller_sgk50_s2tkl/halconf.h
@@ -0,0 +1,9 @@
+// Copyright 2024 JoyLee (@itarze)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define HAL_USE_SPI TRUE
+#define HAL_USE_GPT TRUE
+
+#include_next <halconf.h>
---
+++ keyboards/sharkoon/skiller_sgk50_s2tkl/keymaps/via/config.h
@@ -0,0 +1,4 @@
+// Copyright 2024 JoyLee (@itarze)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
