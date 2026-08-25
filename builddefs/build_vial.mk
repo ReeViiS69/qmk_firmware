ca5c4a0bc697aab84f401b8c72279d2d4938dbf2
ReeViiS69 <wassermann.fabi@gmail.com>
2026-08-26 01:36:14 +0200

    vorbereitung

---
+++ builddefs/build_vial.mk
@@ -0,0 +1,24 @@
+# Vial forward-port for modern QMK
+# Minimal feature set for initial bring-up.
+
+QMK_SETTINGS ?= no
+TAP_DANCE_ENABLE ?= no
+COMBO_ENABLE ?= no
+KEY_OVERRIDE_ENABLE ?= no
+LAYER_LOCK_ENABLE ?= no
+REPEAT_KEY_ENABLE ?= no
+
+SRC += $(QUANTUM_DIR)/vial.c
+
+# The magic serial prefix is used by Vial to identify compatible devices.
+OPT_DEFS += -DVIAL_ENABLE -DSERIAL_NUMBER=\"vial:f64c2b3c\"
+
+ifeq ($(strip $(VIAL_INSECURE)), yes)
+    OPT_DEFS += -DVIAL_INSECURE
+endif
+
+# Generate compressed Vial keyboard definition from vial.json.
+$(QUANTUM_DIR)/vial.c: $(INTERMEDIATE_OUTPUT)/src/vial_generated_keyboard_definition.h
+
+$(INTERMEDIATE_OUTPUT)/src/vial_generated_keyboard_definition.h: $(KEYMAP_PATH)/vial.json
+	python3 util/vial_generate_definition.py $(KEYMAP_PATH)/vial.json $(INTERMEDIATE_OUTPUT)/src/vial_generated_keyboard_definition.h
