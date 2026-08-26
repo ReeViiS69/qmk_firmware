a3169bcdadf33c405575fe09ae0bf59361792b40
ReeViiS69 <wassermann.fabi@gmail.com>
2026-08-26 21:57:32 +0200

    added vial km folder

---
+++ keyboards/sharkoon/skiller_sgk50_s2tkl/keymaps/vial/rules.mk
@@ -0,0 +1,10 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# Nur während des ersten Vial-Bring-ups.
+VIAL_INSECURE = yes
+
+# Deine vorhandenen eigenen RGB-Matrix-Effekte weiterverwenden.
+RGB_MATRIX_CUSTOM_USER = yes
+
+# LTO_ENABLE = yes
